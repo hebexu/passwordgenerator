fa3c269a170f91a503b95099d35d14f7091d2c31
Hebe Xu <hebe.xw@gmail.com>
2020-09-04 22:54:31 +0930

    push

--- script.js
+++ script.js
@@ -11,21 +11,20 @@ function GeneratePassword(length)
   var issymbols = confirm("do you want symbols?")
   var all = uppercase + lowercase + numbers + symbols;
   var password = '';
-  function randomize(isuppercase, islowercase, isnumbers, issymbols, length)
-  { 
-    if(isuppercase === false)
+
+    if(isuppercase == false)
     { 
       uppercase = "";
     }
-    if(islowercase === false)
+    if(islowercase == false)
     {
       lowercase = "";
     }
-    if(isnumbers === false)
+    if(isnumbers == false)
     {
       numbers = "";
     }
-    if (issymbols === false)
+    if(issymbols == false)
     {
       symbols ="";
     }
@@ -33,12 +32,11 @@ function GeneratePassword(length)
     console.log(allCharacter);
     
     for (var i = 0; i < length; i++) {
-     var character = Math.floor(Math.random() * all.length);
-     password += allCharacter.substring(character, character + 1);
+    var character = Math.floor(Math.random() * allCharacter.length);
+    password += allCharacter.substring(character, character + 1);
     }
-  }
-     randomize(isuppercase, islowercase, isnumbers, issymbols, length);
-     var passwordText = document.querySelector("#password");
+
+    var passwordText = document.querySelector("#password");
     passwordText.value = password;
   }
   
