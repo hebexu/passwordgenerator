f4f72a28e7b72c60e251cc0f0d159ccdee11dcc9
Hebe Xu <hebe.xw@gmail.com>
2020-09-04 22:35:12 +0930

    push

--- script.js
+++ script.js
@@ -1,21 +1,45 @@
-
 function GeneratePassword(length)
 {
   var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   var lowercase = 'abcdefghijklmnopqrstuvwxyz';
   var numbers = '0123456789';
   var symbols = '!"#$%&\'()*+,-./:;<=>?@^[\\]^_`{|}~';
+  var length = prompt("what length password would you like?")
+  var isuppercase = confirm("do you want uppercase character?")
+  var islowercase = confirm("do you want lowercase?")
+  var isnumbers = confirm ("do you want numbers?")
+  var issymbols = confirm("do you want symbols?")
   var all = uppercase + lowercase + numbers + symbols;
   var password = '';
-  for (var i = 0; i < length; i++) {
-      var character = Math.floor(Math.random() * all.length);
-      password += all.substring(character, character + 1);
+  function randomize(isuppercase, islowercase, isnumbers, issymbols, length)
+  { 
+    if(isuppercase === false)
+    { 
+      uppercase = "";
+    }
+    if(islowercase === false)
+    {
+      lowercase = "";
+    }
+    if(isnumbers === false)
+    {
+      numbers = "";
+    }
+    if (issymbols === false)
+    {
+      symbols ="";
+    }
+    var allCharacter = lowercase + uppercase + symbols + numbers;
+    console.log(allCharacter);
+    
+    for (var i = 0; i < length; i++) {
+     var character = Math.floor(Math.random() * all.length);
+     password += allCharacter.substring(character, character + 1);
+    }
   }
-  alert("This passsord contains an uppercase");
-  alert("This passsord contains a lowercase");
-  alert("This passsord contains a number");
-  alert("This passsord contains a symbol");
-  alert("This passsord contains 12 length");
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
-}
+     randomize(isuppercase, islowercase, isnumbers, issymbols, length);
+     var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+  }
+  
+
